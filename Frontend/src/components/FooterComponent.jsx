ff539db05111e6cf13f4868fcf4bf976a5104c42
Ankit <ankitkumarraj596@gmail.com>
2025-02-08 11:47:58 +0530

    update

---
+++ Frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+//import from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+          <footer className='footer'>
+             <span>All rights reserved 2025 by Ankit</span>
+          </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
